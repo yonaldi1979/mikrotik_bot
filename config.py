46aa2c4c870dbb2bec6479f2c94584450fce8b25
Yonaldi <34738074+yonaldi1979@users.noreply.github.com>
2025-03-06 00:36:49 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py
 MIKROTIK_ROUTER = {
-    'host': '103.126.116.13',       # Ganti IP Mikrotik
-    'username': 'mtn',
-    'password': 'bejeben123',
+    'host': '192.168.1.1',       # Ganti IP Mikrotik
+    'username': 'apibot',
+    'password': 'passwdapi123',
     'port': 8728,
 }
 
-ADMIN_IDS = [1124439245, 406064056]  # ID Admin yang boleh kirim command
-GROUP_IDS = [-1002356237218]  # ID Group tempat command diterima
+ADMIN_IDS = [123456789, 987654321]  # ID Admin yang boleh kirim command
+GROUP_IDS = [-123456789]  # ID Group tempat command diterima
